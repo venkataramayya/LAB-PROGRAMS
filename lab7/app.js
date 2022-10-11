366ca7180cfecbbad74f20b6e4b22d639108bfff
venkataramayya <94680948+venkataramayya@users.noreply.github.com>
2022-10-11 13:30:41 +0530

    Add files via upload

---
+++ lab7/app.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './Counter';
+import Color from './Color';
+import TextColorDemo from './TextColorDemo';
+function App() {
+  return (
+    <div className="App">
+    <TextColorDemo/>
+      
+    </div>
+  );
+}
+
+export default App;
